39db40940748d2193b9f92fe80063de8f5eb030a
Slava <solovey.1999@mail.ru>
2021-04-12 23:30:23 +0700

    Added business layer with repository for users

---
+++ BusinessLogic/Services/UsersRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BusinessLogic.Interfaces;
+using Domain;
+using Domain.Entities;
+
+namespace BusinessLogic.Services
+{
+    class UsersRepository : IUserRepository
+    {
+        private ApplicationContext context;
+        public UsersRepository(ApplicationContext context)
+        {
+            this.context = context;
+        }
+        public IEnumerable<User> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
